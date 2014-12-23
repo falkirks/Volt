bd82d31a1af71a338d725ac41c2187705ce199f0
Falkirks <bpnoreply@gmail.com>
2014-12-23 10:26:55 -0800

    Added doc comment to InternalMethodException

--- src/volt/exception/InternalMethodException.php
+++ src/volt/exception/InternalMethodException.php
@@ -1,6 +1,12 @@
 <?php
 namespace volt\exception;
-
+/**
+ * Class InternalMethodException
+ * This exception is thrown when a plugin attempts to use a semi-protected method
+ * these methods are not intended to be used through the API and issues may occur
+ * if they are.
+ * @package volt\exception
+ */
 class InternalMethodException extends \Exception{
 
 }
