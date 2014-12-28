86919415c13c4f597bf19432067e641927aaa175
Falkirks <bpnoreply@gmail.com>
2014-12-28 09:45:25 -0800

    WebsiteData can now be used in a foreach loop

--- src/volt/api/WebsiteData.php
+++ src/volt/api/WebsiteData.php
@@ -6,7 +6,7 @@ use volt\exception\PluginNotEnabledException;
 use volt\ServerTask;
 use volt\Volt;
 
-class WebsiteData implements \ArrayAccess, \Countable, \IteratorAggregate{
+class WebsiteData implements \ArrayAccess, \Countable, \Iterator{
     public function __construct(){
         $this->k = 0;
     }
@@ -59,6 +59,62 @@ class WebsiteData implements \ArrayAccess, \Countable, \IteratorAggregate{
         else{
             throw new PluginNotEnabledException;
         }
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next(){
+        $this->k++;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current(){
+        return $this->getIterator()[$this->key()];
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key(){
+        return array_keys($this->getIterator())[$this->k];
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid(){
+        if($this->k < $this->count() && $this->k >= 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind(){
+        $this->k = 0;
     }
 
     public function count(){
